465ee4e80c1baf00a8e6be6186201637c45c0bf1
xpertatgames <croods01@proton.me>
2024-07-06 09:44:59 -0400

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,33 @@
+let firstname;
+firstName = prompt("please tell me your first name:");
+document.write("<h4>FirstName:</h4> " + firstName);
+document.write("<br />")
+document.write("<br />")
+firstName = prompt("please tell me your last name:");
+document.write("<h4>LastName:</h4> " + firstName);
+document.write("<br />")
+document.write("<br />")
+let name;
+firstName = prompt("please tell me your Email:");
+document.write("<h4>Email:</h4> " + firstName);
+document.write("<br />")
+document.write("<br />")
+firstName = prompt("please tell me your Password:");
+document.write("<h4>Password:</h4> " + firstName);
+document.write("<br />")
+document.write("<br />")
+firstName = prompt("please tell me your Adress:");
+document.write("<h4>Adress:</h4> " + firstName);
+document.write("<br />")
+document.write("<br />")
+firstName = prompt("please tell me your phone-number:");
+document.write("<h4>phone-number:</h4> " + firstName);
+document.write("<br />")
+document.write("<br />")
+firstName = prompt("please tell me your card-number:");
+document.write("<h4>card-number:</h4> " + firstName);
+
+function myFunction() {
+    alert("yayy! you got it ");
+    document.write("<button>no</button>")
+  }
